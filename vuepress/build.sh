3cb3b39ec336446d4acdcdfd870cd819af68baa7
CkCode <ckcode@icloud.com>
2022-09-15 18:13:19 +0800

    新建分支

--- vuepress/build.sh
+++ vuepress/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+#set -e
 
 # 生成静态文件
 yarn build
@@ -14,14 +14,12 @@ cd /dist
 
 git init
 git add .
-git commit -m 'deploy'
+git commit -m 'build'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git remote add origin git@github.com:ckcoding/ckcoding.github.io.git
-git branch -M main
-git push -f git@github.com:ckcoding/ckcoding.github.io/ main:gh-pages
+git push -f git@github.com:ckcoding/ckcoding.github.io/ main:Blogs-pages
 
 cd -
