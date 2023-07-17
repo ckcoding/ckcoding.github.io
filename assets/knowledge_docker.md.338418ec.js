db6b9b8f00d78e9fcc1a83730f72bff5de5c40ff
ckcoding <ckcoding@users.noreply.github.com>
2023-07-17 00:40:51 +0000

    deploy: e5c3db307fa8f762a4d12a9048c4b1d7bc247877

---
+++ assets/knowledge_docker.md.338418ec.js
@@ -0,0 +1,40 @@
+import{_ as s,o as n,c as a,d as l}from"./app.c6dbdbd9.js";const g=JSON.parse('{"title":"\u7EAF\u4E2A\u4EBA\u9879\u76EE","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u9752\u9F99\u{1F409}","slug":"\u9752\u9F99\u{1F409}","link":"#\u9752\u9F99\u{1F409}","children":[{"level":3,"title":"\u8BA2\u9605\u5730\u5740","slug":"\u8BA2\u9605\u5730\u5740","link":"#\u8BA2\u9605\u5730\u5740","children":[]},{"level":3,"title":"\u4F9D\u8D56","slug":"\u4F9D\u8D56","link":"#\u4F9D\u8D56","children":[]}]}],"relativePath":"knowledge/docker.md"}'),e={name:"knowledge/docker.md"},p=l(`<h1 id="\u7EAF\u4E2A\u4EBA\u9879\u76EE" tabindex="-1">\u7EAF\u4E2A\u4EBA\u9879\u76EE <a class="header-anchor" href="#\u7EAF\u4E2A\u4EBA\u9879\u76EE" aria-hidden="true">#</a></h1><h2 id="\u9752\u9F99\u{1F409}" tabindex="-1">\u9752\u9F99\u{1F409} <a class="header-anchor" href="#\u9752\u9F99\u{1F409}" aria-hidden="true">#</a></h2><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">docker run -dit \\</span></span>
+<span class="line"><span style="color:#A6ACCD;">  -v /root/docker/ql:/ql/data \\</span></span>
+<span class="line"><span style="color:#A6ACCD;">  -p 5700:5700 \\</span></span>
+<span class="line"><span style="color:#A6ACCD;">  --name qinglong \\</span></span>
+<span class="line"><span style="color:#A6ACCD;">  --hostname qinglong \\</span></span>
+<span class="line"><span style="color:#A6ACCD;">  --restart unless-stopped \\</span></span>
+<span class="line"><span style="color:#A6ACCD;">  whyour/qinglong:latest</span></span>
+<span class="line"><span style="color:#676E95;"># \u542F\u52A8 127.0.0.1:5700</span></span>
+<span class="line"></span></code></pre></div><h3 id="\u8BA2\u9605\u5730\u5740" tabindex="-1">\u8BA2\u9605\u5730\u5740 <a class="header-anchor" href="#\u8BA2\u9605\u5730\u5740" aria-hidden="true">#</a></h3><p><code>CK</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/ckcoding/qinglong.git</span></span>
+<span class="line"></span></code></pre></div><p><code>KingRan</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/KingRan/KR.git</span></span>
+<span class="line"></span></code></pre></div><p><code>he1pu</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/he1pu/JDHelp.git</span></span>
+<span class="line"></span></code></pre></div><p><code>JDHelloWorld</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/JDHelloWorld/jd_scripts.git</span></span>
+<span class="line"></span></code></pre></div><p><code>shufflewzc</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://git.metauniverse-cn.com/https://github.com/shufflewzc/faker2.git</span></span>
+<span class="line"></span></code></pre></div><p><code>leafTheFish</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/leafTheFish/DeathNote.git</span></span>
+<span class="line"></span></code></pre></div><p><code>6dylan6</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/6dylan6/jdpro.git</span></span>
+<span class="line"></span></code></pre></div><p><code>shufflewzc</code></p><div class="language-sh"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">https://github.com/gys619/jdd.git</span></span>
+<span class="line"></span></code></pre></div><h3 id="\u4F9D\u8D56" tabindex="-1">\u4F9D\u8D56 <a class="header-anchor" href="#\u4F9D\u8D56" aria-hidden="true">#</a></h3><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">sablejs</span></span>
+<span class="line"><span style="color:#A6ACCD;">string-random</span></span>
+<span class="line"><span style="color:#A6ACCD;">js-sha256</span></span>
+<span class="line"><span style="color:#A6ACCD;">ts-node</span></span>
+<span class="line"><span style="color:#A6ACCD;">typescript</span></span>
+<span class="line"><span style="color:#A6ACCD;">crypto-js</span></span>
+<span class="line"><span style="color:#A6ACCD;">md5</span></span>
+<span class="line"><span style="color:#A6ACCD;">dotenv</span></span>
+<span class="line"><span style="color:#A6ACCD;">got</span></span>
+<span class="line"><span style="color:#A6ACCD;">ts-md5</span></span>
+<span class="line"><span style="color:#A6ACCD;">tslib</span></span>
+<span class="line"><span style="color:#A6ACCD;">@types/node</span></span>
+<span class="line"><span style="color:#A6ACCD;">requests</span></span>
+<span class="line"><span style="color:#A6ACCD;">tough-cookie</span></span>
+<span class="line"><span style="color:#A6ACCD;">jsdom</span></span>
+<span class="line"><span style="color:#A6ACCD;">download</span></span>
+<span class="line"><span style="color:#A6ACCD;">tunnel</span></span>
+<span class="line"><span style="color:#A6ACCD;">ws</span></span>
+<span class="line"><span style="color:#A6ACCD;">js-base64</span></span>
+<span class="line"><span style="color:#A6ACCD;">date-fns</span></span>
+<span class="line"><span style="color:#A6ACCD;">png-js</span></span>
+<span class="line"><span style="color:#A6ACCD;">axios</span></span>
+<span class="line"><span style="color:#A6ACCD;">nodemailer</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,22),o=[p];function c(t,i,d,r,C,h){return n(),a("div",null,o)}const y=s(e,[["render",c]]);export{g as __pageData,y as default};
---
+++ assets/knowledge_vue.md.c1a2b7ca.js
@@ -0,0 +1,22 @@
+import{_ as s,o as a,c as n,d as l}from"./app.c6dbdbd9.js";const h=JSON.parse('{"title":"vue2","description":"","frontmatter":{},"headers":[{"level":2,"title":"watch","slug":"watch","link":"#watch","children":[]},{"level":2,"title":"computed","slug":"computed","link":"#computed","children":[]}],"relativePath":"knowledge/vue.md"}'),p={name:"knowledge/vue.md"},e=l(`<h1 id="vue2" tabindex="-1">vue2 <a class="header-anchor" href="#vue2" aria-hidden="true">#</a></h1><h2 id="watch" tabindex="-1">watch <a class="header-anchor" href="#watch" aria-hidden="true">#</a></h2><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki"><code><span class="line"><span style="color:#FFCB6B;">watch</span><span style="color:#89DDFF;">:{</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#FFCB6B;">isChange</span><span style="color:#89DDFF;">:{</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#FFCB6B;">immediate</span><span style="color:#89DDFF;">:</span><span style="color:#FF9CAC;">true</span><span style="color:#89DDFF;">,</span><span style="color:#F07178;"> </span><span style="color:#676E95;">//\u521D\u59CB\u5316\u7684\u65F6\u5019\u662F\u5426\u8C03\u7528</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#FFCB6B;">deep</span><span style="color:#89DDFF;">:</span><span style="color:#FF9CAC;">true</span><span style="color:#89DDFF;">,</span><span style="color:#F07178;"> </span><span style="color:#676E95;">//\u662F\u5426\u5F00\u542F\u6DF1\u5EA6\u76D1\u542C</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#82AAFF;">handler</span><span style="color:#F07178;">(</span><span style="color:#A6ACCD;">newValue</span><span style="color:#89DDFF;">,</span><span style="color:#A6ACCD;">oldValue</span><span style="color:#F07178;">)</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div><h2 id="computed" tabindex="-1">computed <a class="header-anchor" href="#computed" aria-hidden="true">#</a></h2><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki"><code><span class="line"><span style="color:#FFCB6B;">computed</span><span style="color:#89DDFF;">:{</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#FFCB6B;">get</span><span style="color:#89DDFF;">:{</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#82AAFF;">test</span><span style="color:#F07178;">()</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#89DDFF;">},</span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#FFCB6B;">set</span><span style="color:#89DDFF;">:{</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#82AAFF;">test</span><span style="color:#F07178;">()</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#F07178;">      </span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">}</span><span style="color:#F07178;"> </span></span>
+<span class="line"><span style="color:#F07178;">  </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div>`,5),o=[e];function c(t,r,F,i,y,d){return a(),n("div",null,o)}const u=s(p,[["render",c]]);export{h as __pageData,u as default};
